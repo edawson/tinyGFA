185c68bf455ca330a73e5f475c3cff47283b1aaf
Eric T Dawson <eric.t.dawson@gmail.com>
2020-04-01 17:17:40 -0400

    Add tests for groups and links

--- test_tinygfa.cpp
+++ test_tinygfa.cpp
@@ -54,21 +54,75 @@ TEST_CASE("GFA 1.0 sequence element parsing handles tags.", "[sequence_elem tags
 }
 
 TEST_CASE("GFA 2.0 sequence element parsing produces valid sequence elements.", "[sequence_elem]"){
-
+    string gfa_line = "S\t64\t7\tACCTGGC";
+	char* gfa_line_raw;
+	pliib::strcopy(gfa_line.c_str(), gfa_line_raw);
+	tgfa::sequence_elem s(gfa_line_raw, 2.0);
+	
+	REQUIRE(s.seq_length == 7);
+	REQUIRE(std::strcmp(s.seq, "ACCTGGC") == 0);
+	REQUIRE(std::strcmp(s.id, "64") == 0);
+	delete [] gfa_line_raw;
 }
 
 
 TEST_CASE("Sequence elements can have both numeric and alphanumeric IDs.", "[sequence_elem]"){
-
+	string gfa_line = "S\tst64\tACCTGGC";
+	char* gfa_line_raw;
+	pliib::strcopy(gfa_line.c_str(), gfa_line_raw);
+	tgfa::sequence_elem s(gfa_line_raw, 1.0);
+	
+	REQUIRE(std::strcmp(s.id, "st64") == 0);
+	delete [] gfa_line_raw;
 }
 
 
 TEST_CASE("Link elements are parsed correctly into edges", "[edge_elem]"){
+	string gfa_line = "L\t1\t+\t2\t-\t0M\tSN:Z:test";
+	char* gfa_line_raw;
+	pliib::strcopy(gfa_line.c_str(), gfa_line_raw);
+	tgfa::edge_elem e(gfa_line_raw);
+
+	REQUIRE(std::strcmp(e.source_id, "1") == 0);
+	REQUIRE(std::strcmp(e.sink_id, "2") == 0);
+	REQUIRE(e.source_orientation_forward == true);
+	REQUIRE(e.sink_orientation_forward == false);
+	REQUIRE(e.source_end() == true);
+	REQUIRE(e.sink_end() == true);
+	REQUIRE(e.source_begin() == true);
+	REQUIRE(e.sink_begin() == true);
+	REQUIRE(std::strcmp(e.alignment, "0M") == 0);
+	REQUIRE(e.get_tag("SN").get_id() == "SN");
 
 }
 
 
 TEST_CASE("GFA 1.0 paths are parsed into groups correctly", "[group_elem]"){
+	
+	string gfa_line = "P\t14ref\t11+,12+,13-\t4M,5M";
+	char* gfa_line_raw;
+	pliib::strcopy(gfa_line.c_str(), gfa_line_raw);
+	tgfa::group_elem g(gfa_line_raw);
+
+	REQUIRE(std::strcmp(g.id, "14ref") == 0);
+	REQUIRE(g.ordered == true);
+	REQUIRE(g.segment_count == 3);
+	REQUIRE(std::strcmp(g.segment_name(0), "11") == 0);
+	REQUIRE(std::strcmp(g.segment_name(1), "12") == 0);
+	REQUIRE(g.segment_orientation(1) == true);
+	REQUIRE(g.segment_orientation(2) == false);
+	REQUIRE(g.overlap_count == 2);
+	REQUIRE(g.overlap_head(1) == "4M");
+	REQUIRE(g.overlap_tail(0) == "4M");
+	REQUIRE(g.overlap_head(2) == "5M");
+
+}
+
+TEST_CASE("GFA 2.0 ordered groups are parsed correctly", "[group_elem]"){
+
+}
+
+TEST_CASE("GFA 2.0 unordered groups are parsed correctly", "[group_elem]"){
 
 }
 
